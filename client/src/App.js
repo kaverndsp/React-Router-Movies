e55cefada3ccbc9ecba4a38f022597607909ab9f
kaverndsp <kaverndsp@users.noreply.github.com>
2020-01-27 18:16:45 -0600

    Adding routes inside of App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import {Route} from "react-router-dom";
+import MovieList from "./Movies/MovieList"
+import Movie from "./Movies/Movie"
 
 import SavedList from './Movies/SavedList';
 
@@ -12,7 +15,17 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+       
+        <Route  exact path="/">
+          <MovieList/>
+        </Route>
+        
+        <Route path="/movies/:movieID">
+          <Movie/>
+        </Route>
+      
+      </div>
     </div>
   );
 };
